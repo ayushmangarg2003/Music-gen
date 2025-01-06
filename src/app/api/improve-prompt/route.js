e11691b8eae7191e54a4f906ee44648993e91651
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2025-01-06 12:48:09 +0530

    Basic Layout Done, Propmt Improv working, music gen left

--- src/app/api/improve-prompt/route.js
+++ src/app/api/improve-prompt/route.js
@@ -2,21 +2,22 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const { prompt } = await req.json();
-
+  const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY
+  const final_prompt = `Improve this prompt for music generation ${prompt}`
+  const MODEL_NAME = 'microsoft/Phi-3-mini-4k-instruct'
   try {
-    const response = await fetch("https://api-inference.huggingface.co/models/gpt2", {
+    const response = await fetch(`https://api-inference.huggingface.co/models/${MODEL_NAME}`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer YOUR_HUGGINGFACE_API_KEY`,
+        Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ inputs: prompt }),
+      body: JSON.stringify({ inputs: final_prompt }),
     });
-
-    if (!response.ok) throw new Error("Failed to improve prompt");
 
     const data = await response.json();
     const improvedPrompt = data[0]?.generated_text || "No suggestions.";
+
     return NextResponse.json({ improvedPrompt });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
--- src/app/api/musicgen/route.js
+++ src/app/api/musicgen/route.js
@@ -2,21 +2,27 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const { prompt } = await req.json();
+  const HUGGINGFACE_API_KEY = process.env.HUGGINGFACE_API_KEY
+  const MODEL_NAME = ''
 
   try {
-    const response = await fetch("https://api-inference.huggingface.co/models/facebook/musicgen-medium", {
+    const response = await fetch(`https://api-inference.huggingface.co/models/${MODEL_NAME}`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer YOUR_HUGGINGFACE_API_KEY`,
+        Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ inputs: prompt }),
     });
 
-    if (!response.ok) throw new Error("Failed to generate music");
+    // console.log("RESPONSEEEEEEE", response);
+
+    console.log(response)
+    // if (!response.ok) throw new Error("Failed to generate music");
 
     const data = await response.json();
     return NextResponse.json(data);
+
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
